63eaddd8e71147fe46bf48c8c85ac156766e91aa
David Michael <devdavem@yahoo.com>
2022-03-29 07:58:48 -0700

    Added assert style to test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var should = require('chai').should();
 var expect = require('chai').expect;
+var assert = require('chai').assert;
+var sinon = require("sinon").sinon;
 var Add = require('../math');
 
 describe('Addition Tests', function() {
@@ -10,7 +12,8 @@ describe('Addition Tests', function() {
         let actualResult = Add(numberOne, numberTwo);
 
         // actualResult.should.equal(expectedResult);
-        expect(actualResult).to.equal(expectedResult);
+        // expect(actualResult).to.equal(expectedResult);
+        assert.equal(actualResult, expectedResult);
     })
 
     it('should NOT return 3 when passed values one and four', function () {
@@ -20,6 +23,7 @@ describe('Addition Tests', function() {
         let actualResult = Add(numberOne, numberTwo);
         
         // actualResult.should.not.equal(expectedResult);
-        expect(actualResult).to.not.equal(expectedResult);
+        // expect(actualResult).to.not.equal(expectedResult);
+        assert.notEqual(actualResult, expectedResult);
     })
 })
